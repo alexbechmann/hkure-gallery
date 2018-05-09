12dc03fd5eff6492e80d53ec6755b85d6c8e615e
Alex Bechmann <alex.bechmann@outlook.com>
2018-05-09 18:36:49 +0200

    from prev

--- react-app/src/shared/root.store.ts
+++ react-app/src/shared/root.store.ts
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { Store } from 'redux';
 import { AppState } from './app.state';
 import { rootReducer } from './root.reducer';
+import * as ReduxPromise from 'redux-promise';
 
-const ReduxPromise = require('redux-promise');
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 export const store: Store<AppState> = createStoreWithMiddleware(rootReducer) as Store<AppState>;
