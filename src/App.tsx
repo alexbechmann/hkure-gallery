f3d387818322afc11302f1ffbd82f8891ef37079
Alex Bechmann <alex.bechmann@outlook.com>
2018-05-09 15:50:08 +0200

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import * as React from 'react';
 import { Paintings } from './paintings/Paintings';
-import { Grid, AppBar, Toolbar, Typography } from 'material-ui';
+import { Grid, AppBar, Toolbar, Typography, MuiThemeProvider } from 'material-ui';
+import { theme } from './styles/theme';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="app">
-        <AppBar position="static" color="primary">
-          <Toolbar>
-            <Typography variant="title" color="inherit">
-              Hkure Gallery
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <div className="container">
-          <Grid container justify="center">
-            <Grid item xs={12} sm={8} lg={6}>
-              <Paintings />
+      <MuiThemeProvider theme={theme}>
+        <div className="app">
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                Hkure Gallery
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <div className="container">
+            <Grid container justify="center">
+              <Grid item xs={12} sm={8} lg={6}>
+                <Paintings />
+              </Grid>
             </Grid>
-          </Grid>
+          </div>
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
