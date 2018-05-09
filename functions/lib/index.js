dcca203260ed8b454b51dbd89da1a710fc90291e
Alex Bechmann <alex.bechmann@outlook.com>
2018-05-09 22:18:14 +0200

    gitignore

--- functions/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions = require("firebase-functions");
-exports.contact = functions.https.onRequest((request, response) => {
-    response.send('Mail sent!');
-});
-//# sourceMappingURL=index.js.map
